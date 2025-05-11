d4df9f0f2860661123ab2b37d706d52ab9b80995
craig white <craig.t.w@hotmail.co.uk>
2025-05-11 23:57:29 +0100

    Chapter - Practicing With Methods

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,11 +5,11 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     Rigidbody rb;
     
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        PrintInstructions();
     }
 
     // Update is called once per frame
@@ -20,13 +20,25 @@ public class Mover : MonoBehaviour
 
     void FixedUpdate()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
         // String comes from Unity Input Manager in project settings
         float xValue = Input.GetAxis("Horizontal") * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.AddForce (xValue, yValue, zValue);
-   
+        rb.AddForce(xValue, yValue, zValue);
+
         rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, moveSpeed);
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Use WASD or Arrow keys to move.");
+        Debug.Log("Dont bump into the objects!");
     }
 }
