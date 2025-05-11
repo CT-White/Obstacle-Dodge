1fe2cf8e5aa72a924c64ed9f63f3bd37953b00ad
craig white <craig.t.w@hotmail.co.uk>
2025-05-11 23:44:35 +0100

    Chapter - Basic Collisions I also modified the player object to use a rigidbody with addforce instead of translate to fix collision errors

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,21 +3,30 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
+    Rigidbody rb;
     
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void FixedUpdate()
+    {
         // String comes from Unity Input Manager in project settings
-        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float xValue = Input.GetAxis("Horizontal") * moveSpeed;
         float yValue = 0f;
-        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        // Move the object by the specified values
-        transform.Translate(xValue, yValue, zValue);
+        float zValue = Input.GetAxis("Vertical") * moveSpeed;
+
+        rb.AddForce (xValue, yValue, zValue);
+   
+        rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, moveSpeed);
     }
 }
